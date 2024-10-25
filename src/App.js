ef66de72c4fc15188e86bc48139ed2c0581ebac5
miguelcastroe <135630596+miguelcastroe@users.noreply.github.com>
2024-10-25 16:09:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,25 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Purpose from './pages/Purpose';
 import Problem from './pages/Problem';
 import Review from './pages/Review';
 
 function App() {
-  // Estado para almacenar los datos ingresados por el usuario
   const [data, setData] = useState({});
 
   return (
     <Router basename="/canvasdepensamiento">
       <div>
-        {/* Encabezado de la aplicación */}
         <header>
           <h1>Web App de Pensamiento para IA</h1>
         </header>
-        {/* Definir las rutas de la aplicación */}
-        <Switch>
-          <Route path="/" exact>
-            <Welcome />
-          </Route>
-          <Route path="/purpose">
-            <Purpose data={data} setData={setData} />
-          </Route>
-          <Route path="/problem">
-            <Problem data={data} setData={setData} />
-          </Route>
-          <Route path="/review">
-            <Review data={data} />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/purpose" element={<Purpose data={data} setData={setData} />} />
+          <Route path="/problem" element={<Problem data={data} setData={setData} />} />
+          <Route path="/review" element={<Review data={data} />} />
+        </Routes>
       </div>
     </Router>
   );
