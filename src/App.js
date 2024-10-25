3670380f5884f58ae96f726b461d25ef0d67b694
miguelcastroe <135630596+miguelcastroe@users.noreply.github.com>
2024-10-25 15:50:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,26 +6,34 @@ import Problem from './pages/Problem';
 import Review from './pages/Review';
 
 function App() {
-    const [data, setData] = useState({});
+  // Estado para almacenar los datos ingresados por el usuario
+  const [data, setData] = useState({});
 
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact>
-                    <Welcome />
-                </Route>
-                <Route path="/purpose">
-                    <Purpose data={data} setData={setData} />
-                </Route>
-                <Route path="/problem">
-                    <Problem data={data} setData={setData} />
-                </Route>
-                <Route path="/review">
-                    <Review data={data} />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router basename="/canvasdepensamiento">
+      <div>
+        {/* Encabezado de la aplicación */}
+        <header>
+          <h1>Web App de Pensamiento para IA</h1>
+        </header>
+        {/* Definir las rutas de la aplicación */}
+        <Switch>
+          <Route path="/" exact>
+            <Welcome />
+          </Route>
+          <Route path="/purpose">
+            <Purpose data={data} setData={setData} />
+          </Route>
+          <Route path="/problem">
+            <Problem data={data} setData={setData} />
+          </Route>
+          <Route path="/review">
+            <Review data={data} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
